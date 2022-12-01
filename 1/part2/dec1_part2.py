5baa9df0d8da8cad517aa98237aa9354d1777ae8
SimonM <smerlin@unis.fr>
2022-12-01 09:20:09 +0100

    add day 1 answers fot both parts

---
+++ 1/part2/dec1_part2.py
@@ -0,0 +1,24 @@
+import sys
+import os
+f = open(os.path.join(sys.path[0], './data.txt'), 'r')
+lines = [l.rstrip() for l in f.readlines()]
+
+import sys
+import os
+f = open(os.path.join(sys.path[0], './data.txt'), 'r')
+lines = [l.rstrip() for l in f.readlines()]
+
+calories = []
+
+#compute calories for each Elf
+cpt=0
+for l in lines:
+    if l == "":
+        calories.append(cpt)
+        cpt=0
+    else:
+        cpt += int(l)
+
+# print the Elf carrying the most calories
+calories.sort(reverse=True)
+print(sum(calories[:3]))
