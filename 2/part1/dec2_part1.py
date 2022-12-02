c1ccf134d64b67e6b6c3dd40c1844dfaa293ab50
SimonM <smerlin@unis.fr>
2022-12-02 09:52:52 +0100

    add day 2 answers for both parts

---
+++ 2/part1/dec2_part1.py
@@ -0,0 +1,29 @@
+import sys
+import os
+f = open(os.path.join(sys.path[0], './data.txt'), 'r')
+lines = [l.rstrip() for l in f.readlines()]
+
+
+#shapes values
+shapesValues = {
+    "X":1,
+    "Y":2,
+    "Z":3
+}
+
+#games values
+gamesValues = {
+    "A X":3,
+    "A Y":6,
+    "A Z":0,
+    "B X":0,
+    "B Y":3,
+    "B Z":6,
+    "C X":6,
+    "C Y":0,
+    "C Z":3
+}
+
+score = sum([gamesValues[l] + shapesValues[l[2]] for l in lines])
+
+print(score)
---
+++ 2/part2/dec2_part2.py
@@ -0,0 +1,29 @@
+import sys
+import os
+f = open(os.path.join(sys.path[0], './data.txt'), 'r')
+lines = [l.rstrip() for l in f.readlines()]
+
+
+#games values
+gamesValues = {
+    "X":0, #lose
+    "Y":3, #draw
+    "Z":6  #win
+}
+
+#shapes values
+choosenShapeValue = {
+    "A X":3,
+    "A Y":1,
+    "A Z":2,
+    "B X":1,
+    "B Y":2,
+    "B Z":3,
+    "C X":2,
+    "C Y":3,
+    "C Z":1
+}
+
+score = sum([choosenShapeValue[l] + gamesValues[l[2]] for l in lines])
+
+print(score)
