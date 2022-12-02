366537cb640e63bb033a4d6da2173bb1e4f21383
SimonM <smerlin@unis.fr>
2022-12-02 09:58:57 +0100

    extract data file from each part and update template

--- 1/part2/dec1_part2.py
+++ 1/part2/dec1_part2.py
@@ -1,11 +1,6 @@
 import sys
 import os
-f = open(os.path.join(sys.path[0], './data.txt'), 'r')
-lines = [l.rstrip() for l in f.readlines()]
-
-import sys
-import os
-f = open(os.path.join(sys.path[0], './data.txt'), 'r')
+f = open(os.path.join(sys.path[0], './../data.txt'), 'r')
 lines = [l.rstrip() for l in f.readlines()]
 
 calories = []
